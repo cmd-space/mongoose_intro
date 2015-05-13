e682898a2c8f5bac6f6a925812ad3b93b387868f
Mason Crane <masonecrane@gmail.com>
2015-05-13 23:49:21 +0000

    server started, integrated mongoose, created index.ejs

--- server.js
+++ server.js
@@ -1,84 +1,39 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+// require the path module
+var path = require("path");
+// require express and create the express app
+var express = require("express");
+var app = express();
+// require bodyParser since we need to handle post data for adding a user
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+mongoose.connect('mongodb://basic_mongoose');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+app.use(bodyParser.urlencoded());
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// set the views folder and set up ejs
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+var UserSchema = new mongoose.Schema({
+ name: String,
+ age: Number
+})
+var User = mongoose.model('User', UserSchema);
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
+// root route
+app.get('/', function(req, res) {
+ // This is where we would get the users from the database and send them to the index view to be displayed.
+ res.render('index');
+})
+// route to add a user
+app.post('/users', function(req, res) {
+ console.log("POST DATA", req.body);
+ // This is where we would add the user from req.body to the database.
+ res.redirect('/');
+})
+// listen on 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+})
